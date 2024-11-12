ed1aab0e46e76217a818531145d22b3dfce33cf2
ArtyomShumaylov <shumaylov2000@inbox.ru>
2024-11-12 15:13:32 +0300

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,12 @@
+document.getElementById('calculateBtn').addEventListener('click', function() {
+    const quantity = parseInt(document.getElementById('quantity').value);
+    const productPrice = parseFloat(document.getElementById('product').value);
+    
+    if (!quantity || quantity < 1) {
+        document.getElementById('result').textContent = 'Введите корректное количество товара.';
+        return;
+    }
+
+    const totalCost = quantity * productPrice;
+    document.getElementById('result').textContent = `Стоимость заказа: ${totalCost} руб.`;
+});
